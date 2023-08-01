4833d97ffb868271b4090614099540d4779e63d8
aak-1989 <Korotonozhkoaa@yandex.ru>
2023-08-01 12:54:53 +0300

    обновление

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL = 'https://5883dffe-3d44-4673-8092-7c3922ecc9f7.serverhub.praktikum-services.ru/'
+URL = 'https://ddc5517c-cc9f-40f5-ab0c-0f338f73a9b3.serverhub.praktikum-services.ru/'
 # end_point
 CREATE_AN_ORDER = 'api/v1/orders'
 ACCEPT_THE_ORDER = 'v1/orders/track?t='
