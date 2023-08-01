0b21370030dcbad05a5e35933be3cb32202c0700
aak-1989 <Korotonozhkoaa@yandex.ru>
2023-08-01 09:26:19 +0300

    Проверил теорию с .сору непрокатило

--- request_create.py
+++ request_create.py
@@ -8,9 +8,19 @@ def create_new():
                          json=creating_an_order).json()['track']
 
 cn = create_new()
+cn_s = str(cn)
+
+
+# cn_l = list(cn)
+#
+# def copy_cn():
+#     cop = cn_l.copy()
+#     return cop
+
+
 # в задании было сохронить, какой ТЗ такой ХЗ.
 track = open('track.txt', 'w')
-track.write(str(cn))
+track.write(cn_s)
 track.close()
 
 
@@ -33,4 +43,4 @@ track.close()
 
 
 def test_accept_the_order():
-    return requests.get(URL + ACCEPT_THE_ORDER + str(cn)).status_code
+    return requests.get(URL + ACCEPT_THE_ORDER + cn_s).status_code
