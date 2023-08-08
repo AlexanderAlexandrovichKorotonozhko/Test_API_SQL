5a08f30624372f3bb08287f79b3385c27ef05197
aak-1 <Korotonozhkoaa@yandex.ru>
2023-08-08 21:30:10 +0300

    поменялась ссылка после проверки

--- configuration.py
+++ configuration.py
@@ -1,6 +1,4 @@
-URL = 'https://ddc5517c-cc9f-40f5-ab0c-0f338f73a9b3.serverhub.praktikum-services.ru/'
+URL = 'https://dde28c5c-0864-40ad-aa0d-15ca9979220b.serverhub.praktikum-services.ru/'
 # end_point
 CREATE_AN_ORDER = 'api/v1/orders'
 ACCEPT_THE_ORDER = 'v1/orders/track?t='
-
-
